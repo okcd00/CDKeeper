e850c8a1974ab2b2e32359e0631949d20c15d04a
okcd00 <okcd00@qq.com>
2021-03-18 10:25:44 +0800

    Update: Codeforces Round 708 Div 2.

---
+++ Templates/codeforces_tpl.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+def input(): 
+    return sys.stdin.readline().rstrip()
+def read_int_list(): 
+    return list(map(int, input().split()))
+def read_int_tuple(): 
+    return tuple(map(int, input().split()))
+def read_int(): 
+    return int(input())
+
