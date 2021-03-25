792561d1086a95eb1e0603065f8edbde48daf422
okcd00 <okcd00@qq.com>
2021-03-26 01:02:50 +0800

    Update: CF Round 710 Div3.

---
+++ Codeforces/20210326 #710 Div 3/C. Double-ended Strings.py
@@ -0,0 +1,82 @@
+import os
+import sys 
+from io import BytesIO, IOBase
+ 
+
+class FastIO(IOBase):
+    newlines = 0
+    BUFSIZE = 8192
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, self.BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, self.BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+def read_int_list(): 
+    return list(map(int, input().split()))
+def read_int_tuple(): 
+    return tuple(map(int, input().split()))
+def read_int(): 
+    return int(input())
+
+
+def lcstring(s1, s2):
+    len1 = len(s1)
+    len2 = len(s2)
+    res = [[0 for i in range(len1+1)] for j in range(len2+1)]
+    result = 0
+    for i in range(1, len2+1):
+        for j in range(1, len1+1):
+            if s2[i-1] == s1[j-1]:
+                res[i][j] = res[i-1][j-1]+1
+                result = max(result, res[i][j])  
+    return result
+
+
+# Problem 
+t = read_int()
+for _ in range(t):
+    a = input()
+    b = input()
+    ans = len(a) + len(b) - 2 * lcstring(a, b)
+    print(ans)
