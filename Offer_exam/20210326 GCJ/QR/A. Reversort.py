2b6a6b7ae3b29cb2dbc4111c6942b714f365d7f3
okcd00 <okcd00@qq.com>
2021-03-26 22:07:39 +0800

    Update: store modifies for GCJ.

---
+++ Offer_exam/20210326 GCJ/QR/A. Reversort.py
@@ -0,0 +1,80 @@
+import os
+import sys 
+from io import BytesIO, IOBase
+ 
+
+class FastIO(IOBase):
+    newlines = 0
+    BUFSIZE = 8192
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, self.BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, self.BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+def read_int_list(): 
+    return list(map(int, input().split()))
+def read_int_tuple(): 
+    return tuple(map(int, input().split()))
+def read_int(): 
+    return int(input())
+
+
+# Problem 
+t = read_int()
+for case_idx in range(t):
+    n = read_int()
+    a = read_int_list()
+    ans = 0
+    for idx in range(n-1):
+        if a[idx] == idx + 1:
+            ans += 1
+            continue
+        pivot = a.index(idx + 1)
+        # print('>', a, pivot, ans)
+        # print(a[:idx], a[idx: pivot+1][::-1], a[pivot+1:])
+        a = a[:idx] + a[idx: pivot+1][::-1] + a[pivot+1:]
+        # print(idx, pivot)
+        ans += (pivot - idx + 1)
+    print("Case #{}: {}".format(case_idx + 1, ans))
+
