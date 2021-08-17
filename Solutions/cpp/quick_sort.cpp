626a662ff0eb737fd5d2302402829d6f103eec99
okcd00 <okcd00@qq.com>
2021-08-17 09:14:55 +0800

    Update: add main function for quick sort.

--- Solutions/cpp/quick_sort.cpp
+++ Solutions/cpp/quick_sort.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 //严蔚敏《数据结构》标准分割函数
 int Paritition1(int A[], int low, int high)
 {
@@ -26,5 +30,15 @@ void QuickSort(int A[], int low, int high) //快排母函数
     int pivot = Paritition1(A, low, high);
     QuickSort(A, low, pivot - 1);
     QuickSort(A, pivot + 1, high);
+  }
+}
+
+int main()
+{
+  int A[10]{1,9,2,3,4,6,6,6,7,8}; 
+  QuickSort(A, 0, 9);
+  for(int i=0; i<10; i++)
+  {
+    cout << A[i] << endl;
   }
 }
