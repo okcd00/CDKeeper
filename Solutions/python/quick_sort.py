1f82bcb14e87a462065eec99da3ff044831afefe
Dian Chen <okcd00@users.noreply.github.com>
2021-11-22 15:54:33 +0800

    Update: quick sort and quick selection.

--- Solutions/python/quick_sort.py
+++ Solutions/python/quick_sort.py
@@ -1,17 +1,37 @@
 def quick_sort_simple(data):
     # sort the list
     if len(data) >= 2:
+        pivot = data[0]  # any number is fine 
+        left, right = [], []
+        for num in data[1:]:
+            if num >= pivot:
+                right.append(num)
+            else:
+                left.append(num)
+        return quick_sort(left) + [pivot] + quick_sort(right)
+    else:
+        return data
+
+
+def quick_find_simple(data, k):
+    # find the k-th smallest number
+    if len(data) >= 2:
         # any number is fine
-        mid = data[0]  
+        mid = data[0]
         left, right = [], []
         for num in data[1:]:
             if num >= mid:
                 right.append(num)
             else:
                 left.append(num)
-        return quick_sort(left) + [mid] + quick_sort(right)
+        if len(left) == k - 1:
+            return mid
+        elif len(left) < k - 1:
+            return quick_find_simple(right, k=k-len(left)-1)
+        else:  # len(left) > k - 1
+            return quick_find_simple(left, k=k)
     else:
-        return data
+        return data[0]
 
 
 def quick_sort(data_list, l=None, r=None):
@@ -44,28 +64,46 @@ def quick_sort(data_list, l=None, r=None):
     return data_list
 
 
-def quick_find(data, k):
-    # find the k-th smallest number
-    if len(data) >= 2:
-        # any number is fine
-        mid = data[0]
-        left, right = [], []
-        for num in data[1:]:
-            if num >= mid:
-                right.append(num)
-            else:
-                left.append(num)
-        if len(left) == k - 1:
-            return mid
-        elif len(left) < k - 1:
-            return quick_find(right, k=k-len(left)-1)
-        else:  # len(left) > k - 1
-            return quick_find(left, k=k)
-    else:
-        return data[0]
+class QuickSort:
+    def __init__(self):
+        self.partition = self.partition_oneway
+
+    def find_kth_largest(self, nums: List[int], k: int) -> int:
+        return self.quick_select(nums, 0, len(nums)-1, len(nums)-k)
+
+    def find_kth_smallest(self, nums: List[int], k: int) -> int:
+        return self.quick_select(nums, 0, len(nums)-1, k-1)
+
+    def quick_select(self, nums, l, r, index):
+        pivot = self.partition(nums, l, r)
+
+        if pivot < index:
+            return self.quick_select(nums, pivot+1, r, index)
+        elif pivot > index:
+            return self.quick_select(nums, l, pivot-1, index)
+        else:
+            return nums[pivot]
+
+    def quick_sort(self, nums, l, r):
+        # self.quick_sort(nums, 0, len(nums)-1)
+        if l < r: 
+            pivot = self.partition(nums, l, r) 
+            self.quick_sort(nums, l, pivot-1) 
+            self.quick_sort(nums, pivot+1, r) 
+
+    def partition_oneway(self, nums, l, r):
+        # print(nums)
+        pivot, v = l, nums[r]  # l can be randomly selected
+        for idx in range(l, r):
+            if nums[idx] <= v:
+                nums[pivot], nums[idx] = nums[idx], nums[pivot]
+                pivot += 1
+                # print("->", nums)
+        nums[pivot], nums[r] = nums[r], nums[pivot]
+        return pivot
 
 
 if __name__ == "__main__":
     dat = [2,3,4,51,1,1,53,26,3,4,16]
     print(quick_sort(dat))
-    print(quick_find(dat, k=3))
+    print(quick_find_simple(dat, k=3))
