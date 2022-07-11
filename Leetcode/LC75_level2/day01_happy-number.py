880ae1e2ba07e3da45073177190a07dd50daee78
Dian Chen <okcd00@qq.com>
2022-07-11 23:58:54 +0800

    Update: LeetCode training Day1.

---
+++ Leetcode/LC75_level2/day01_happy-number.py
@@ -0,0 +1,38 @@
+"""
+https://leetcode.cn/problems/happy-number/
+编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」 定义为：
+对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+如果这个过程 结果为 1，那么这个数就是快乐数。
+如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+"""
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        cycle_members = {4, 16, 37, 58, 89, 145, 42, 20}
+
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        while n != 1 and n not in cycle_members:
+            n = get_next(n)
+        return n == 1
+
+    def isHappy_cd(self, n: int) -> bool:
+        rec = [n]
+        cur = sum([int(c)**2 for c in str(n)])
+        while True:
+            cur = sum([int(c)**2 for c in str(rec[-1])])
+            # print(cur)
+            if cur == 1:
+                return True
+            if cur in rec:
+                return False
+            rec.append(cur)
---
+++ Leetcode/LC75_level2/day01_spiral-matrix.py
@@ -0,0 +1,25 @@
+"""
+https://leetcode.cn/problems/spiral-matrix/
+给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+"""
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ret = []
+        u, d = 0, len(matrix) - 1
+        l, r = 0, len(matrix[0]) - 1
+        while True:
+            ret.extend(matrix[u][l:r+1])
+            u += 1
+            if u > d: break
+            ret.extend([matrix[k][r] for k in range(u, d+1)])
+            r -= 1
+            if r < l: break
+            ret.extend(matrix[d][l:r+1][::-1])
+            d -= 1
+            if d < u: break
+            ret.extend([matrix[k][l] for k in range(u, d+1)][::-1])
+            l += 1
+            if l > r: break
+        return ret
