340eef6f9216b3d321c89fb663e65b3a75e9ec96
Dian Chen <okcd00@qq.com>
2022-09-05 00:32:38 +0800

    Update: add prime records.

---
+++ Solutions/python/get_prime_numbers.py
@@ -0,0 +1,20 @@
+def init_prime_euler():
+    maxn = int(1e6)  # int(1e7)
+    prime_list = [False] * (maxn+1)
+
+    ret = []
+    for i in range(2, maxn+1):
+        if not prime_list[i]:
+            ret.append(i)
+        
+        for j in ret:
+            if i * j > maxn:
+                break
+            prime_list[i*j] = True
+            if i % j == 0:
+                break
+    return ret
+
+
+if __name__ == "__main__":
+    print(init_prime_euler()[:20])
