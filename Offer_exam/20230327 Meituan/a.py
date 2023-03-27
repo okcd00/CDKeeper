7647089a962d531673b810047845ec13a4976725
Dian Chen <okcd00@qq.com>
2023-03-27 23:51:10 +0800

    Update: correct the codes.

--- Offer_exam/20230327 Meituan/a.py
+++ Offer_exam/20230327 Meituan/a.py
@@ -1,5 +1,10 @@
 #coding=utf-8
-import sys 
+
+"""
+1 1 2 3 5 8 13 21 34 55
+2 3 5 8 13 21 34 55 89 144
+5
+"""
 
 a = list(map(int, input().strip().split()))
 b = list(map(int, input().strip().split()))
@@ -12,31 +17,28 @@ def get_kth_smallest(k):
     offset_a = 0
     offset_b = 0
     while True:
+        if offset_a >= n:
+            return b[offset_b + k - 1]
+        if offset_b >= m:
+            return a[offset_a + k - 1] 
         if k == 1:
             return min(a[offset_a], b[offset_b])
         
         index_a = min(offset_a + k // 2 - 1, n - 1)
         index_b = min(offset_b + k // 2 - 1, m - 1)
-        
-        if offset_a >= n:
-            return b[k - n + 1]
-        if offset_b >= m:
-            return a[k - m + 1] 
-        
-        if a[offset_a] > b[offset_b]:
+        if a[index_a] <= b[index_b]:
             k -= index_a - offset_a + 1
             offset_a = index_a + 1
         else:
             k -= index_b - offset_b + 1
             offset_b = index_b + 1        
 
-            
-if __name__ == "__main__":
-    if n+m % 2 == 1:
-        ans = get_kth_smallest((n+m) // 2 - 1)
-    else:
-        l = get_kth_smallest((n+m) // 2 - 1)
-        r = get_kth_smallest((n+m) // 2)
-        ans = (l + r) / 2
-    print(ans)
+
+if total_size % 2 == 1:
+    ans = get_kth_smallest( (total_size + 1) // 2 )
+else:
+    l = get_kth_smallest( total_size // 2 )
+    r = get_kth_smallest( total_size // 2 + 1 )
+    ans = (l + r) / 2
+print(ans)
     
