1edfc5fe807b2611014dc4e91c445dd78dd55662
Dian Chen <okcd00@qq.com>
2023-12-03 19:24:43 +0800

    Update: correct the answer for Microsoft offer exam.

--- Offer_exam/20220921 Microsoft/rotate_array.py
+++ Offer_exam/20220921 Microsoft/rotate_array.py
@@ -1,27 +1,32 @@
-def rotate_array(nums, k):
-    # nums = list(map(int, input().strip().split()))
-    n = len(nums)
-    k = k % n
+class Solution:
+    def _gcd(self, a, b):
+        if a == b:
+            return a
+        if a > b:
+            a, b = b, a
+        return self._gcd(a, b-a)
 
-    if n % k != 0:
-        pivot = k
-        tmp = nums[k]  # 0 + k
-        for _ in range(n-1):
-            pre_index = (pivot - k + n) % n
-            nums[pivot] = nums[pre_index]
-            pivot = pre_index
-        nums[pivot] = tmp
-    else:
-        for start_position in range(k):
-            pivot = start_position + k
-            tmp = nums[pivot]
-            for _ in range(n-1):
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # nums = list(map(int, input().strip().split()))
+        n = len(nums)
+        k = k % n
+        if k == 0:
+            return nums
+        for start_position in range(self._gcd(n, k)):
+            pivot = start_position
+            tmp = nums[start_position]
+            for _ in range(n//self._gcd(n, k)-1):
                 pre_index = (pivot - k + n) % n
                 nums[pivot] = nums[pre_index]
                 pivot = pre_index
             nums[pivot] = tmp
-    return nums        
+        return nums
+
 
 
 if __name__ == "__main__":
-    print(rotate_array([1,2,3,4,5,6,7], k=3))
+    s = Solution()
+    print(s.rotate_array([1,2,3,4,5,6,7], k=3))
